ce5e0cff20449c619b134d2911bdd6cfede73997
Junghoon-P <qkr0454@naver.com>
2021-07-05 17:44:08 +0900

    Add: 초기세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import SiginUp from "./pages/SignUp/SignUp";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import Shop from "./pages/Shop/Shop";
+import Detail from "./pages/Detail/Detail";
+
+import "./styles/common.scss";
+import "./styles/reset.scss";
+
+class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/signup" component={SiginUp} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/detail" component={Detail} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class detail extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
