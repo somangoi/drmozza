bcb349bc6ede029d6e8a906699ba65ea1fc3b85c
Junghoon-P <qkr0454@naver.com>
2021-07-05 20:04:42 +0900

    Add: 초기세팅 3차

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './Routes';
 
-ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Routes />, document.getElementById('root'));
