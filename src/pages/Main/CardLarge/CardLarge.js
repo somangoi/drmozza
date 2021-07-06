a08b44afc20120a7ada326d7e98823a36d814947
Junghoon-P <qkr0454@naver.com>
2021-07-06 20:32:49 +0900

    Add: main페이지 레이아웃 작업중

---
+++ src/pages/Main/CardLarge/CardLarge.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './CardLarge.scss';
+
+export default class CardLage extends Component {
+  render() {
+    return <div>여기는 카드!</div>;
+  }
+}
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import CardLarge from './CardLarge/CardLarge';
+import './Main.scss';
 
 export default class Login extends Component {
   render() {
-    return <div></div>;
+    return (
+      <>
+        <div className="nav">네브 컴포넌트 자리</div>
+        <body>
+          <div className="slideFst">슬라이드1</div>
+          <CardLarge /> //기존 카드 컴포넌트 재활용
+          <div className="slogan">슬로건</div>
+          <div className="slideScd">슬라이드2</div>
+          <div className="cheeseAd">광고</div>
+        </body>
+        <div className="footer">푸터 자리</div>
+      </>
+    );
   }
 }
