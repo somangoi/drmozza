2d5a46223e4610115ef1ce3c777e8994043745a7
Jongmin Lee <jongminlee8910@gmail.com>
2021-07-08 17:42:54 +0900

    [Add] Login 페이지 Nav 및 Footer CSS 수정, reset.scss 수정

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 
-import './styles/common.scss';
 import './styles/reset.scss';
+import './styles/common.scss';
 import './styles/variable.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
