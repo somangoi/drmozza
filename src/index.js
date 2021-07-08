26aeae9a318b015146b14a7cc4b5cbdc4541d181
Jongmin Lee <jongminlee8910@gmail.com>
2021-07-08 18:09:40 +0900

    [Add] 폰트 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Routes from './Routes';
 
 import './styles/reset.scss';
-import './styles/common.scss';
 import './styles/variable.scss';
+import './styles/common.scss';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
