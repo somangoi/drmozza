e9d129745918be75d6c91a698076cc946a6f445e
Somi Hwang <somihwang@Somis-MacBook-Pro.local>
2021-07-15 17:45:22 +0900

    ADD: sorting 기능 구현

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -19,7 +19,7 @@ export default class Shop extends Component {
       milkList: [],
       styleList: [],
       countriesList: [],
-      selectedOption: 'price-descending',
+      selectedOption: '',
     };
   }
 
@@ -53,7 +53,9 @@ export default class Shop extends Component {
 
   componentDidUpdate(prevProps) {
     if (this.props.match.params.id !== prevProps.match.params.id) {
-      fetch(`${API}/categories/${this.props.match.params.id}`)
+      fetch(
+        `${API}/categories/${this.props.match.params.id}&offset=1&limit=10&sort=${this.state.selectedOption}`
+      )
         .then(res => res.json())
         .then(data =>
           this.setState({
@@ -71,64 +73,79 @@ export default class Shop extends Component {
     }
   }
 
+  sortingHandler = e => {
+    this.setState({ selectedOption: e.target.value });
+
+    fetch(
+      `${PRODUCT_API}?category=${this.props.match.params.id}&sort=${e.target.value}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          productList: data.results,
+        });
+      });
+  };
+
   render() {
+    console.log(this.state.productList);
     const { currentCategory, productList, milkList, styleList, countriesList } =
       this.state;
+
+    console.log('productList', productList);
+
     return (
       <div>
-        {isValidObject(currentCategory) && (
-          <main className="shopContainer">
+        <main className="shopContainer">
+          {isValidObject(currentCategory) && (
             <CategoryImg currentCategory={currentCategory} />
-            <section className="shopBody">
-              <aside className="shopAside">
-                <ul className="sideMenuTop">
-                  <Link to="/shop/14">
-                    <li>SHOP ALL</li>
-                  </Link>
-                  <Link to="/shop/15">
-                    <li>BEST SELLERS</li>
-                  </Link>
-                </ul>
-                <SideMenuList
-                  milkList={milkList}
-                  styleList={styleList}
-                  countriesList={countriesList}
-                />
-              </aside>
-              <article className="products">
-                <div className="sortSection">
-                  <div className="sortBlank"></div>
-                  <div className="sortContainer">
-                    <div className="sortWrap">
-                      <span className="sortText">SORT BY</span>
-                      <span className="sortBar">
-                        <select
-                          name="sort"
-                          className="sort"
-                          onChange={option => this.handleChange(option)}
-                        >
-                          <option
-                            value="price-descending"
-                            onChange={this.sortPriceDesc}
-                          >
-                            Price, High to Low
-                          </option>
-                          <option
-                            value="price-ascending"
-                            onChange={this.sortPriceAsc}
-                          >
-                            Price, Low to High
-                          </option>
-                        </select>
-                      </span>
-                    </div>
+          )}
+          <section className="shopBody">
+            <aside className="shopAside">
+              <ul className="sideMenuTop">
+                <Link to="/shop/14">
+                  <li>SHOP ALL</li>
+                </Link>
+                <Link to="/shop/15">
+                  <li>BEST SELLERS</li>
+                </Link>
+              </ul>
+              <SideMenuList
+                milkList={milkList}
+                styleList={styleList}
+                countriesList={countriesList}
+              />
+            </aside>
+            <article className="products">
+              <div className="sortSection">
+                <div className="sortBlank"></div>
+                <div className="sortContainer">
+                  <div className="sortWrap">
+                    <span className="sortText">SORT BY</span>
+                    <span className="sortBar">
+                      <select
+                        name="sort"
+                        className="sort"
+                        onChange={this.sortingHandler}
+                      >
+                        <option value="price-descending">
+                          Price, High to Low
+                        </option>
+                        <option value="price-ascending">
+                          Price, Low to High
+                        </option>
+                      </select>
+                    </span>
                   </div>
                 </div>
+              </div>
+              {productList.length > 0 && (
                 <ProductList productList={productList} />
-              </article>
-            </section>
-          </main>
-        )}
+              )}
+            </article>
+          </section>
+        </main>
+
         <Footer />
       </div>
     );
